e379c7bf839a0ef4e6ea39d98b15adae6758658e
alejandro <alejandrober96@gmail.com>
2023-09-11 14:37:59 -0500

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+
+  resources :directors, only: [:show, :index] do
+    resources :groups
+  end
+
+  resources :users, only: [:show, :create, :update, :delete]
+  resources  :professions
+  resources :jobs
+  resources :jobplaces
+
 end
