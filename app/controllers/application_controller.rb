2f1e938c071ed9175b16f1d5fd7424854ffa789a
alejandro <alejandrober96@gmail.com>
2023-09-11 17:57:46 -0500

    Add image to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   protected 
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :email, :password) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :email, :password, :current_password) }
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:full_name, :image, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:full_name, :image, :email, :password, :current_password) }
   end
 end
