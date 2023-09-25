c7895b51c5eaa763c0e0f72ec0557aa53bb0760d
alejandro <alejandrober96@gmail.com>
2023-09-25 12:05:31 -0500

    Add filter by profession

--- app/javascript/controllers/group_show_controller.js
+++ app/javascript/controllers/group_show_controller.js
@@ -2,16 +2,49 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect(){
-    console.log('hello, group')
     const selectProfession = document.querySelector(".select-profession");
     const selectJobplace = document.querySelector(".select-jobplace")
     const selectJob = document.querySelector(".select-job")
     const searchBtn = document.querySelector(".search-btn")
     const cleanBtn = document.querySelector(".clean-btn")
+    const professions = document.querySelectorAll(".profession")
+    const searchResults = document.querySelector(".search-results")
     let profession, jobplace, job ;
+    let counter = 0;
+
+    function searchProfession(param){
+      if(param == undefined) {
+        return;
+      }
+      professions.forEach(prof => {
+        let filterChild = []
+        prof.childNodes.forEach(child => {
+          if(child.localName == 'li'){
+            filterChild.push(child.innerText)
+          }})
+          for(let i = 0; i < filterChild.length ; i++){
+          if(filterChild[i].toLowerCase() == param.toLowerCase()){
+            counter += 1;
+            prof.parentElement.parentElement.parentElement.style.display = "flex"
+            break;
+          }else {
+            prof.parentElement.parentElement.parentElement.style.display = "none"
+          }
+        }
+      });
+    }
+
+    function clean(){
+      professions.forEach( prof => {
+        prof.parentElement.parentElement.parentElement.style.display = "flex"
+      } )
+      counter = 0;
+      searchResults.style.display = "none";
+    }
 
     selectProfession.addEventListener('change', () => {
       profession = selectProfession.value
+      clean();
     })
     
     selectJobplace.addEventListener('change', () => {
@@ -24,7 +57,20 @@ export default class extends Controller {
     })
     
     searchBtn.addEventListener('click', () => {
-      console.log(profession, jobplace, job)
+      console.log(profession, jobplace, job);
+      if(profession != undefined){
+        searchProfession(profession);
+      }
+      if (profession == undefined && jobplace == undefined && job == undefined) {
+        searchResults.style.display = "block";
+        searchResults.innerText = `Seleccione un parametro de búsqueda`
+        setTimeout(() => {
+          searchResults.style.display = "none";
+        }, 2000)
+      } else {
+        searchResults.style.display = "block";
+        searchResults.innerText = `Se han encontrado: ${counter} resultados`
+      }
     })
     
     cleanBtn.addEventListener('click', () => {
@@ -34,6 +80,7 @@ export default class extends Controller {
       job = 'undefined' 
       jobplace = 'undefined' 
       profession = 'undefined' 
+      clean();
     })
   }
 }
