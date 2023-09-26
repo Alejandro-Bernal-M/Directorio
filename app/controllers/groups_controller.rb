6ed000b60700262ef4d9950a485d5d6e61ae46cb
alejandro <alejandrober96@gmail.com>
2023-09-26 11:54:55 -0500

    Add create and new to the group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :check_logged 
+  before_action :check_logged, only: [:show]
 
   def check_logged
     unless current_director || current_user
@@ -29,5 +29,29 @@ class GroupsController < ApplicationController
     @jobsusers = JobsUser.all
     @users = @group.users
     @users_processed = @users.map {|user| {user: user, jobs: user.jobs.map{|job| {job: job, responsibilities: @responsibilities.where(jobs_user_id: @jobsusers.where(job_id: job.id, user_id: user.id))}}, professions: user.professions,  jobplaces: user.jobplaces }}
+  end
+
+  def new
+    @group = Group.new
+  end
+  
+
+  def create
+    @group = Group.new(group_params)
+    
+    respond_to do |format|
+      if @group.save
+        format.html{redirect_to director_path(current_director), notice: 'Grupo creado'}
+      else
+        format.html{ render :new, status: :unprocessable_entity}
+      end
+    end
+  end
+  
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :slogan, :color, :director_id)
   end
 end
