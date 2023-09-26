909c9c867969dc1730c2841963dc7232a4a56b33
alejandro <alejandrober96@gmail.com>
2023-09-26 16:32:33 -0500

    Add crud methods for directors_controller

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -18,4 +18,30 @@ class DirectorsController < ApplicationController
   # rescue ActiveRecord::RecordNotFound
   #   render html: '<h1>Not found</h1>'.html_safe
   end
+
+  def create
+    @director = Director.new(director_params)
+
+    if @director.save 
+      redirect_to superusers_path, notice: "Director creado con exito"
+    else
+      redirect_to superusers_path, alert: "Error creando el director"
+    end
+  end
+  
+  def destroy
+    @director = Director.find(params[:id])
+    
+    if @director.destroy
+      redirect_to superusers_path, notice: "Director eliminado con exito"
+    else
+      redirect_to superusers_path, alert: "Error eliminando el director"
+    end
+  end
+
+  protected
+
+  def director_params
+    params.require(:director).permit(:username, :email, :password, :password_confirmation)
+  end
 end
