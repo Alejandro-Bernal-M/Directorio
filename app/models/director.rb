2dbcd816991693dd3c480791517212e279c84363
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-10-02 14:05:04 -0500

    Add relationship between plan and director

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,6 +1,7 @@
 class Director < ApplicationRecord
   has_secure_password
   has_many :groups, dependent: :destroy
+  has_one :plan
 
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
