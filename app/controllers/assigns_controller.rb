f90890dacd7185d9b75c134dd60770935fec9ba2
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-10-04 17:10:57 -0500

    Add updating plan

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -3,11 +3,23 @@ class AssignsController < ApplicationController
     @director = Director.find(params[:director_id])
     @plan = Plan.find(params[:selected_plan_id])
 
-    if @director.update(plan: @plan)
-      redirect_to director_path(current_director), notice: "Plan asignado"
-    else 
-      redirect_to director_path(current_director), alert: "Error asignando el plan"
-    end
+    @directorplan = @director.directors_plan
 
+    if @directorplan
+      if @directorplan.plan == @plan
+        redirect_to director_path(current_director), alert: "Este es su plan actual"
+      else
+        ActiveRecord::Base.connection.execute("DELETE FROM directors_plans WHERE plan_id = #{@directorplan.plan_id} AND director_id = #{@director.id}")
+        DirectorsPlan.create(plan: @plan, director: @director)
+        redirect_to director_path(current_director), notice: "Plan actualizado"
+      end
+    else
+      @director.build_directors_plan(plan: @plan)
+      if @director.save
+        redirect_to director_path(current_director), notice: "Plan asignado"
+      else
+        redirect_to director_path(current_director), alert: "Error asignando el plan"
+      end
+    end
   end
 end
