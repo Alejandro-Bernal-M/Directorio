4979808da97133737b3e50e48fd76cf56eaf9932
Alejandro Bernal Marin <alejandrober96@gmail.com>
2023-10-04 16:27:02 -0500

    Add rendering radio buttons

--- db/migrate/20231003204625_add_price_to_plan.rb
+++ db/migrate/20231003204625_add_price_to_plan.rb
@@ -1,5 +1,7 @@
 class AddPriceToPlan < ActiveRecord::Migration[7.0]
   def change
     add_column :plans, :price, :integer
+    add_foreign_key :directors_plans, :plans, on_delete: :cascade
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -152,6 +152,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_03_204625) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "directors_plans", "plans", on_delete: :cascade
   add_foreign_key "groups", "directors"
   add_foreign_key "groups_users", "groups"
   add_foreign_key "groups_users", "users"
